80e08d51fbc556ec7bb530644a52f7ab33006156
yejinyou <yejinyou@Christines-MacBook-Pro.local>
2017-06-26 11:59:19 +0100

    d

--- json2cwl.py
+++ json2cwl.py
@@ -1,13 +1,34 @@
 import requests
 import pprint
+import os
+
+
 
 #for now we would be importing the url manually but there should be a way to take inputs and put that url into get
-#ie. take in url as a string then r = requests.get(url)
-r = requests.get('https://software.broadinstitute.org/gatk/documentation/tooldocs/current/org_broadinstitute_gatk_tools_walkers_haplotypecaller_HaplotypeCaller.php.json')
-doc = r.json()
 
+#import the json url manually
+r = requests.get('https://software.broadinstitute.org/gatk/documentation/tooldocs/current/org_broadinstitute_gatk_tools_walkers_haplotypecaller_HaplotypeCaller.php.json')
+jsonf = r.json()
+
+fname = jsonf['name']+'.cwl' #set file name
+fpath = 'c:/home/yejinyou/PY'
+
+filepath = os.path.join(fpath,fname)
+if not os.path.exists(fpath):
+	os.makedirs(fpath)
+f = open(filepath, 'a')
+
+#fi = open("C:\\example.txt","w+")
+#print(fi)
+#ie. take in url as a string then r = requests.get(url)
+
+
+g = open("cwl.txt","w+")
 cwl = {}
-cwl['id'] = doc['name']
+cwl['id'] = jsonf['name']
+
+#for args in f['arguments']:
+	#print(args)
 #activeregion
 #annotfield
 #annotinfo
@@ -46,5 +67,5 @@ cwl['id'] = doc['name']
 
 
 
-print(doc['name'])
-print(cwl)
+#print(jsonf['name'])
+#print(cwl)
