6341b42bb98bd3cf1f3be0c1900128abdd47d91d
Pramoda Karnati <pkarnati@mit.edu>
2017-07-14 15:06:19 +0100

    update generate_cwl

--- generate_cwl.py
+++ generate_cwl.py
@@ -6,8 +6,7 @@ import json
 import sys
 from bs4 import BeautifulSoup
 import pprint
-
-# import json2cwl.py
+import json2cwl
 
 
 def prepare_json_links(version):
@@ -15,6 +14,7 @@ def prepare_json_links(version):
     data = requests.get(url).text
     soup = BeautifulSoup(data, "html.parser")
     url_list = []
+    
     for sub in soup.find_all('tr'):
         for child in sub.find_all('td'):
             for a in child.find_all('a', href = True):
@@ -23,33 +23,53 @@ def prepare_json_links(version):
     return url_list
 
 
-url = "https://software.broadinstitute.org/gatk/documentation/tooldocs/3.5-0/"
-
-r = requests.get(url)
-data = r.text
-
-soup = BeautifulSoup(data, "html.parser")
-#print(soup)
-
-url_list = []
-
-for sub in soup.find_all('tr'):
-    for child in sub.find_all('td'):
-        for a in child.find_all('a', href = True):
-            url_list.append(a['href'] + ".json") if "Exception" not in a['href'] else ''
+def convert_json_files(fromdir, url_list):
+    directory = os.path.join(fromdir, 'jsonfolder')
+    todir = os.path.join(fromdir, 'cwlfiles')
+    for tool in url_list:
+        os.chdir(directory)
+        json_1 = url + tool
+        r = requests.get(json_1).json()
+        fname = r['name'] + '.json'
+        f = open(fname, 'a')
+        f.write(json.dumps(r, indent = 4, sort_keys = False))
+        f.close()
+        json2cwl.make_cwl(directory, todir, fname)
 
 current = os.getcwd()
-directory = os.path.join(current, 'jsonfolder')
-os.makedirs(directory)
-os.chdir(directory)
 
-for tool in url_list:
-    json_1 = url + tool
-    r = requests.get(json_1).json()
-    fname = r['name'] + '.json'
-    f = open(fname, 'a')
-    f.write(json.dumps(r, indent = 4, sort_keys = False))
-    f.close()
+url_list = prepare_json_links(3.5)
+convert_json_files(current, url_list)
+
+# url = "https://software.broadinstitute.org/gatk/documentation/tooldocs/3.5-0/"
+
+# r = requests.get(url)
+# data = r.text
+
+# soup = BeautifulSoup(data, "html.parser")
+# #print(soup)
+
+# url_list = []
+
+# for sub in soup.find_all('tr'):
+#     for child in sub.find_all('td'):
+#         for a in child.find_all('a', href = True):
+#             url_list.append(a['href'] + ".json") if "Exception" not in a['href'] else ''
+
+# current = os.getcwd()
+# directory = os.path.join(current, 'jsonfolder')
+# os.makedirs(directory)
+# os.chdir(directory)
+
+# for tool in url_list:
+#     json_1 = url + tool
+#     r = requests.get(json_1).json()
+#     fname = r['name'] + '.json'
+#     f = open(fname, 'a')
+#     f.write(json.dumps(r, indent = 4, sort_keys = False))
+#     f.close()
+
+
 
 
 # json_1 = url + url_list[0]
