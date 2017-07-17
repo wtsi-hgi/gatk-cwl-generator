f4833abfa7993701fd1be5ed49f192a4c369884e
yejinyou <yejinyou@Christines-MacBook-Pro.local>
2017-07-17 16:53:41 +0100

    m

--- working.py
+++ working.py
@@ -18,47 +18,6 @@ import os
 import json
 
 
-
-#=======================================================================================
-#helps form a commandline
-def need_def(arg):
-    if 'List' in arg['type']:
-        if arg['defaultValue'] == '[]' or arg['defaultValue'] == 'NA':
-            arg['defaultValue'] = []
-        else:
-            arg['defaultValue'] = [str(a) for a in arg['defaultValue'][1:-1].split(',')]
-    if arg['defaultValue'] == '[]' or arg['defaultValue'] == 'NA':
-        return False
-    if ('boolean' in arg['type'] or 'List' in arg['type']) or 'false' in arg['defaultValue']:
-        return True
-    return False
-
-
-def output_writer(args,outputs=[]):
-  if 'writer' in args['type'].lower():
-    outpt = {'id': args['name'], 'type': ['null','File'], 'outputBinding':{'glob':'$(inputs.'+args['name'][2:]+')'}}
-    outputs.append(outpt)
-
-def command_line_writer(args,comLine=""):
-    if need_def(args):
-        comLine += "$(defHandler('" + args['synonyms'] + "', WDLCommandPart('NonNull(inputs." + args['name'].strip("-") + ")', " + str(args['defaultValue'])  + "))) "
-    else:
-        if args['defaultValue'] != "NA" and args['defaultValue'] != "none":
-           comLine += args['synonyms'] + " $(WDLCommandPart('NonNull(inputs." + args['name'].strip("-") + ")', '" + args['defaultValue'] + "')) "
-        elif args['synonyms'] == '-o':
-           comLine += "$(defHandler('" + args['synonyms'] + "', WDLCommandPart('NonNull(inputs." + args['name'].strip("-") + ")', "+"'stdout'"+"))) "
-        else:
-           comLine += "$(WDLCommandPart('\"" + args['synonyms'] + "\" + NonNull(inputs." + args['name'].strip("-") + ")', ' ')) " 
-
-
-
-#=======================================================================================
-
-
-
-
-
-
 #import the json url manually (gatk 3.5-0 version)
 r = requests.get('https://software.broadinstitute.org/gatk/documentation/tooldocs/3.5-0/org_broadinstitute_gatk_tools_walkers_haplotypecaller_HaplotypeCaller.php.json').json()
 d = requests.get('https://software.broadinstitute.org/gatk/documentation/tooldocs/3.5-0/org_broadinstitute_gatk_engine_CommandLineGATK.php.json').json()
@@ -95,10 +54,6 @@ cwl = {'id':jsonf['name'],
                                             """function secondary_files(f) { return typeof f; if (f.indexOf('.cram')!= -1 ){ return '.crai';} 
                                             else if (f.search('.bam')) { return '.bai'; } else if (f.search('.fa')) { return ['.fai','^.dict']; }}"""]},
                        { "dockerPull": "gatk:latest","class": "DockerRequirement"}]}
-
-
-
-
 
 
 
