673adcf86f2852bbd2a1745fca146e0a17eccce2
yejinyou <yejinyou@Christines-MacBook-Pro.local>
2017-07-17 16:07:06 +0100

    d

--- json2cwl.py
+++ json2cwl.py
@@ -155,11 +155,19 @@ def need_def(arg):
 
 def output_writer(args,outputs=[]):
   if 'writer' in args['type'].lower():
-    outpt = {}
-    outpt['id'] = args['name']
-    outpt['type'] = ['null','File']
-    outpt['outputBinding'] = {'glob':'$(inputs.'+args['name'][2:]+')'}
+    outpt = {'id': args['name'], 'type': ['null','File'], 'outputBinding':{'glob':'$(inputs.'+args['name'][2:]+')'}}
     outputs.append(outpt)
+
+def command_line_writer(args,comLine=""):
+    if need_def(args):
+        comLine += "$(defHandler('" + args['synonyms'] + "', WDLCommandPart('NonNull(inputs." + args['name'].strip("-") + ")', " + str(args['defaultValue'])  + "))) "
+    else:
+        if args['defaultValue'] != "NA" and args['defaultValue'] != "none":
+           comLine += args['synonyms'] + " $(WDLCommandPart('NonNull(inputs." + args['name'].strip("-") + ")', '" + args['defaultValue'] + "')) "
+        elif args['synonyms'] == '-o':
+           comLine += "$(defHandler('" + args['synonyms'] + "', WDLCommandPart('NonNull(inputs." + args['name'].strip("-") + ")', "+"'stdout'"+"))) "
+        else:
+           comLine += "$(WDLCommandPart('\"" + args['synonyms'] + "\" + NonNull(inputs." + args['name'].strip("-") + ")', ' ')) " 
 
 #converts json to cwl
 def cwlf_generator(item,cwlf):
@@ -210,15 +218,7 @@ def cwlf_generator(item,cwlf):
       inputs.append(inpt)
       
       ###TURN TO COMMANDLINE FUNCTION   
-      if need_def(args):
-          comLine += "$(defHandler('" + args['synonyms'] + "', WDLCommandPart('NonNull(inputs." + args['name'].strip("-") + ")', " + str(args['defaultValue'])  + "))) "
-      else:
-          if args['defaultValue'] != "NA" and args['defaultValue'] != "none":
-             comLine += args['synonyms'] + " $(WDLCommandPart('NonNull(inputs." + args['name'].strip("-") + ")', '" + args['defaultValue'] + "')) "
-          elif args['synonyms'] == '-o':
-             comLine += "$(defHandler('" + args['synonyms'] + "', WDLCommandPart('NonNull(inputs." + args['name'].strip("-") + ")', "+"'stdout'"+"))) "
-          else:
-             comLine += "$(WDLCommandPart('\"" + args['synonyms'] + "\" + NonNull(inputs." + args['name'].strip("-") + ")', ' ')) " 
+      command_line_writer(args,comLine)
       
       if 'requires' in args['fulltext'] and 'index' in args['fulltext']:
         print(args['name'])
