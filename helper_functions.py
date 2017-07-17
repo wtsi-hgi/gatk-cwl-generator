b16b6c4137f4cf76fe434d41f602ed5368596499
yejinyou <yejinyou@Christines-MacBook-Pro.local>
2017-07-17 17:03:16 +0100

    hf

--- helper_functions.py
+++ helper_functions.py
@@ -2,3 +2,15 @@ def output_writer(args,outputs):
   if 'writer' in args['type'].lower():
     outpt = {'id': args['name'], 'type': ['null','File'], 'outputBinding':{'glob':'$(inputs.'+args['name'][2:]+')'}}
     outputs.append(outpt)
+
+
+def commandline_writer(args,comLine)
+  if need_def(args):
+      comLine += "$(defHandler('" + args['synonyms'] + "', WDLCommandPart('NonNull(inputs." + args['name'].strip("-") + ")', " + str(args['defaultValue'])  + "))) "
+  else:
+      if args['defaultValue'] != "NA" and args['defaultValue'] != "none":
+         comLine += args['synonyms'] + " $(WDLCommandPart('NonNull(inputs." + args['name'].strip("-") + ")', '" + args['defaultValue'] + "')) "
+      elif args['synonyms'] == '-o':
+         comLine += "$(defHandler('" + args['synonyms'] + "', WDLCommandPart('NonNull(inputs." + args['name'].strip("-") + ")', "+"'stdout'"+"))) "
+      else:
+         comLine += "$(WDLCommandPart('\"" + args['synonyms'] + "\" + NonNull(inputs." + args['name'].strip("-") + ")', ' ')) " 
