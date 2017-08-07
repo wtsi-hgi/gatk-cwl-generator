0d26d149bef3ecff5e5b44d843f1584e7240c811
ThomasHickman <Thomas.Hickman42@gmail.com>
2017-08-07 11:41:13 +0100

    Cleaning up json2cwl

--- gatkcwlgenerator/json2cwl.py
+++ gatkcwlgenerator/json2cwl.py
@@ -8,9 +8,10 @@ from helper_functions import *
 
 invalid_args = ['--help', '--defaultBaseQualities']
 
+
 def cwl_generator(json_, cwl, cmd_line_options):
     """
-    Converts GATK tools in .json to .cwl (this function changes the cwl parameter)
+    Converts GATK tools documented in .json to .cwl (this function changes the cwl parameter)
 
     Arguments below are classified as invalid for following reasons:
         --DBQ: holds invalid default
@@ -21,8 +22,10 @@ def cwl_generator(json_, cwl, cmd_line_options):
     """
     com_line = ""
     outputs = []
-    inputs = [{"doc": "Index file of reference genome", "type": "File", "id": "refIndex"},
-               { "doc": "dict file of reference genome", "type": "File", "id": "refDict"}]
+    inputs = [
+        {"doc": "Index file of reference genome", "type": "File", "id": "refIndex"},
+        {"doc": "dict file of reference genome", "type": "File", "id": "refDict"}
+    ]
 
     for argument in json_['arguments']:
         if not argument['name'] in invalid_args:
@@ -31,25 +34,38 @@ def cwl_generator(json_, cwl, cmd_line_options):
     cwl["inputs"] = inputs
     cwl["outputs"] = outputs
     if com_line:
-      cwl["arguments"] = [{ "shellQuote": False,
-                           "valueFrom": com_line }]
+        cwl["arguments"] = [{
+            "shellQuote": False,
+            "valueFrom": com_line
+        }]
+
 
 def json2cwl(GATK_json, cwl_dir, cmd_line_options):
     """
     Make a cwl file with a given GATK json file in the cwl directory
     """
 
-    skeleton_cwl = {'id': GATK_json['name'],
-           'cwlVersion': 'v1.0',
-           'baseCommand': ['java','-jar','/gatk/GenomeAnalysisTK.jar'],
-           'class': 'CommandLineTool',
-           'requirements': [{"class": "ShellCommandRequirement"},
-                            {"class": "InlineJavascriptRequirement",
-                            "expressionLib": [
-                                "function getFileArgs(f, a){if(a == undefined){return ' ' + f}else{return ':' + a + ' ' + f}}" # Allows you to add annotations
-                                # TODO: make this more readable
-                            ]},
-                            {"dockerPull": "gatk:latest", "class": "DockerRequirement"}]}
+    skeleton_cwl = {
+        'id': GATK_json['name'],
+        'cwlVersion': 'v1.0',
+        'baseCommand': ['java', '-jar', '/gatk/GenomeAnalysisTK.jar'],
+        'class': 'CommandLineTool',
+        'requirements': [
+            {
+                "class": "ShellCommandRequirement"
+            }, {
+                "class": "InlineJavascriptRequirement",
+                "expressionLib": [
+                    # Allows you to add annotations
+                    "function getFileArgs(f, a){if(a == undefined){return ' ' + f}else{return ':' + a + ' ' + f}}"
+                    # TODO: make this more readable
+                ]
+            }, {
+                "dockerPull": "gatk:latest",
+                "class": "DockerRequirement"
+            }
+        ]
+    }
 
     # Create and write the cwl file
     fname = GATK_json['name'] + '.cwl'
@@ -60,5 +76,6 @@ def json2cwl(GATK_json, cwl_dir, cmd_line_options):
         skeleton_cwl,
         cmd_line_options
     )
-    f.write(json.dumps(skeleton_cwl, indent=4, sort_keys=False))  # write the file
+    f.write(json.dumps(skeleton_cwl, indent=4,
+                       sort_keys=False))  # write the file
     f.close()
