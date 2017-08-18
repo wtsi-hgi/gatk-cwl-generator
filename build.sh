fd01b3366f94c25c9620aac8dbceaf25ce9e47fa
ThomasHickman <Thomas.Hickman42@gmail.com>
2017-08-18 11:41:00 +0100

    Corrected build script

--- build.sh
+++ build.sh
@@ -3,10 +3,9 @@ VERSIONS="3.5
 3.7
 3.8"
 
-PYTHONPATH=.
 
 for ver in $VERSIONS
 do
-    python gatkcwlgenerator -v $ver "$@"
+    PYTHONPATH=. python gatkcwlgenerator -v $ver "$@"
 done
-zip gatk_cmdline_tools gatk_cmdline_tools/*
+zip -r gatk_cmdline_tools gatk_cmdline_tools
