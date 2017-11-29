424f61740fe04b3a55e6251256cfc8cd3565d657
Joshua C. Randall <jcrandall@alum.mit.edu>
2017-11-29 17:53:57 +0000

    switch to building in tmpdir

--- build.sh
+++ build.sh
@@ -7,11 +7,16 @@ VERSIONS="3.5
 3.7
 3.8"
 
+tmpdir=$(mktemp -d)
+builddir="${tmpdir}/gatk_cmdline_tools"
+mkdir -p "${builddir}"
 
 for ver in $VERSIONS
 do
-    PYTHONPATH=. python gatkcwlgenerator -v $ver "$@"
+    PYTHONPATH=. python gatkcwlgenerator -v ${ver} -o "${builddir}/${ver}" "$@"
 done
-zip -r gatk_cmdline_tools gatk_cmdline_tools
-tar zcvf gatk_cmdline_tools.tgz gatk_cmdline_tools
-tar jcvf gatk_cmdline_tools.tar.bz2 gatk_cmdline_tools
+(cd "${tmpdir}"; zip -r gatk_cmdline_tools gatk_cmdline_tools)
+(cd "${tmpdir}"; tar zcvf gatk_cmdline_tools.tgz gatk_cmdline_tools)
+(cd "${tmpdir}"; tar jcvf gatk_cmdline_tools.tar.bz2 gatk_cmdline_tools)
+
+rm -rf "${tmpdir}"
