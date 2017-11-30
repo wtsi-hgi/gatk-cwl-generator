83c773d66eea1c58c8bd6a60cec5be8cb2e0099b
Joshua C. Randall <jcrandall@alum.mit.edu>
2017-11-30 11:49:59 +0000

    add temporary build virtualenv to build.sh

--- build.sh
+++ build.sh
@@ -10,6 +10,17 @@ VERSIONS=( 3.5 3.6 3.7 3.8 4.beta-latest )
 tarbase="gatk-cwl-generator-${generator_version}-gatk_cmdline_tools"
 
 tmpdir=$(mktemp -d)
+python_bin=$(which python)
+echo "Using ${python_bin} to generate temporary virtualenv ${tmpdir}/venv"
+${python_bin} -m virtualenv "${tmpdir}/venv"
+echo "Activating virtualenv in ${tmpdir}/venv"
+set +u # virtualenv activate script references unset vars
+. "${tmpdir}/venv/bin/activate"
+set -u
+
+echo "Installing requirements in virtualenv"
+pip install -r requirements.txt
+
 builddir="${tmpdir}/${tarbase}"
 mkdir -p "${builddir}"
 echo "Building CWL in ${builddir} for GATK versions ${VERSIONS[@]}"
@@ -19,6 +30,9 @@ do
     echo "Generating CWL for GATK version ${ver}"
     PYTHONPATH=. python gatkcwlgenerator -v ${ver} -o "${builddir}/${ver}" "$@"
 done
+
+echo "Deactivating virtualenv"
+deactivate
 
 echo "Generating zip file"
 ( cd "${tmpdir}"; zip -r ${tarbase} ${tarbase} )
