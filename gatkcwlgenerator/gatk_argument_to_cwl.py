da74b42bfafdbf1ab37b3d7a0803f54eb29d71b0
Josh Holland <jh36@sanger.ac.uk>
2018-07-30 14:39:31 +0100

    Remove TODO

--- gatkcwlgenerator/gatk_argument_to_cwl.py
+++ gatkcwlgenerator/gatk_argument_to_cwl.py
@@ -70,7 +70,7 @@ def GATK_type_to_CWL_type(gatk_type: str) -> CWLType:
         return CWLStringType()
     elif gatk_type in map(str.lower, OUTPUT_TYPE_FILE_EXT.keys()):
         # insert this in here and replace it later
-        return CWLStringType()  # TODO: should this be CWLFileType?
+        return CWLStringType()
     elif "intervalbinding" in gatk_type:
         return CWLUnionType(
             CWLFileType(),
